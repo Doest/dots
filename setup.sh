cb5e4030c89bff2bac8c5e28f9b6207c751cb563
Mads Doest <madsdoest@gmail.com>
2024-10-29 13:42:25 +0100

    Added ohmszsh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ apt_packages=(
 	htop
 	curl
 	neovim
+	zsh
+	tmux
 	build-essential
 	software-properties-common
 	ca-certificates
@@ -70,6 +72,27 @@ fc-cache -fv
 
 ## End nerdfonts
 
+##
+## Oh-my-zsh
+##
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## End Oh-my-zsh
+
+
+##
+## Regolith
+##
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_2-ubuntu-noble-amd64 noble main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+sudo apt update
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-gruvbox
+
+## End Regolith
 
 ##
 ## Docker Setup
