2e11043d6d29a963eb220f264b306d68fde306e7
Mads <madsdoest@gmail.com>
2025-09-16 10:45:41 +0200

    Update update-systemd-resolved.sh

--- update-systemd-resolved.sh
+++ update-systemd-resolved.sh
@@ -1,43 +1,33 @@
 #!/bin/bash
 
-# A script to integrate OpenVPN with systemd-resolved
-#
-# This script is designed to be called by OpenVPN's 'up' and 'down' options.
-#
-# When the tunnel comes up, it sets the DNS servers and search domains
-# for the VPN's network interface using resolvectl.
-#
-# When the tunnel goes down, it reverts the interface's DNS settings.
-
 set -e
 
-# The network interface used by the VPN tunnel (e.g., tun0)
 DEV=$1
-# The script type given by OpenVPN ('up' or 'down')
 SCRIPT_TYPE=$2
-
-# Environment variables passed by OpenVPN
-# foreign_option_1='dhcp-option DNS 10.10.1.1'
-# foreign_option_2='dhcp-option DOMAIN vpn.mycompany.com'
 
 case $SCRIPT_TYPE in
   up)
-    # Flush current DNS settings for the interface
-    resolvectl dns "$DEV"
-    resolvectl domain "$DEV"
-
     # Set DNS servers and domains from OpenVPN's pushed options
+    DNS_SERVERS=()
+    DOMAINS=()
     for option in ${!foreign_option_*}
     do
-      read -r _ proto family dns_ip <<< "${!option}"
+      read -r _ proto family value <<< "${!option}"
       if [[ "$proto" == "dhcp-option" && "$family" == "DNS" ]]; then
-        resolvectl dns "$DEV" "$dns_ip"
+        DNS_SERVERS+=("$value")
       fi
       if [[ "$proto" == "dhcp-option" && "$family" == "DOMAIN" ]]; then
-        # The '~' makes this a routing-only domain
-        resolvectl domain "$DEV" "~$dns_ip"
+        DOMAINS+=("~$value")
       fi
     done
+
+    # Set the DNS servers and domains for the interface
+    resolvectl dns "$DEV" "${DNS_SERVERS[@]}"
+    resolvectl domain "$DEV" "${DOMAINS[@]}"
+
+    # This line prevents DNS leaks by telling systemd-resolved
+    # that no other DNS servers should be used for this interface.
+    resolvectl dnsscope "$DEV" link-local
     ;;
   down)
     # Revert all DNS changes for the interface
